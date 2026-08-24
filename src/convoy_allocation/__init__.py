e64d2988e9b28fb2add98af40613c65e3ab58600
Jors Academy <abalci16@posta.pau.edu.tr>
2026-08-25 00:01:33 +0300

    Export CVaR allocation API

--- src/convoy_allocation/__init__.py
+++ src/convoy_allocation/__init__.py
@@ -2,19 +2,23 @@
 
 from .model import AllocationResult, Convoy, Escort, solve_allocation
 from .uncertainty import (
+    CVaRAllocationResult,
     ScenarioOutcome,
     ThreatScenario,
     UncertainAllocationResult,
+    solve_cvar_allocation,
     solve_uncertain_allocation,
 )
 
 __all__ = [
     "AllocationResult",
+    "CVaRAllocationResult",
     "Convoy",
     "Escort",
     "ScenarioOutcome",
     "ThreatScenario",
     "UncertainAllocationResult",
     "solve_allocation",
+    "solve_cvar_allocation",
     "solve_uncertain_allocation",
 ]
